fe13c8cffd572936601d917ea182199b305c6446
EkBratchik <71023472+EkBratchik@users.noreply.github.com>
2020-09-24 09:12:08 +0300

    Update Main.java

--- src/main/java/com/bsu/Main.java
+++ src/main/java/com/bsu/Main.java
@@ -9,38 +9,30 @@ public class Main {
             String phrase1 = sc.nextLine();
             System.out.println(phrase1);
 
-            String test_text = "qwe kjfd qwe qwe\nasd asd\nzxc kfdjsklfj zxc";
-            System.out.println(test_text);
-            System.out.println("****************");
-
             Scanner k = new Scanner(System.in);
             System.out.print("Input a number: ");
             int num = k.nextInt();
 
-            String formatted_text = remove_first_words(phrase1, num);
-            String formatted_text2 = remove_first_words(test_text, num);
+            String formattedText = remove_first_words(phrase1, num);
 
             System.out.println("****************");
-            System.out.print(formatted_text);
-            System.out.println("****************");
-            System.out.println(formatted_text2);
-
+            System.out.print(formattedText);
         }
 
         public static String remove_first_words(String text, int count){
-            StringBuilder new_text = new StringBuilder();
+            StringBuilder newText = new StringBuilder();
             for (String line : text.split("\n")) {
-                String[] all_words = line.split(" ");
+                String[] allWords = line.split(" ");
 
-                if (all_words.length <= count) {
+                if (allWords.length <= count) {
                     continue;
                 }
 
-                String[] needed_words = Arrays.copyOfRange(all_words, count, all_words.length);
-                new_text.append(String.join(" ", needed_words));
-                new_text.append("\n");
+                String[] neededWords = Arrays.copyOfRange(allWords, count, allWords.length);
+                newText.append(String.join(" ", neededWords));
+                newText.append("\n");
             }
-            return new_text.toString();
+            return newText.toString();
         }
 
     }
